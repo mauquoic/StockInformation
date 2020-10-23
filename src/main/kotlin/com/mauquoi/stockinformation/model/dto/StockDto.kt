2c37ca6ad8378e88ec87639fa3d0d956267f1bf0
mauquoic <mauquoic@gmail.com>
2020-10-23 21:31:18 +0200

    remove unnecessary class

--- src/main/kotlin/com/mauquoi/stockinformation/model/dto/StockDto.kt
+++ src/main/kotlin/com/mauquoi/stockinformation/model/dto/StockDto.kt
@@ -6,12 +6,6 @@ data class StockDto(val name: String,
                     val symbol: String,
                     val lookup: String)
 
-data class StockDetailsDto(val name: String,
-                           val symbol: String,
-                           val currency: Currency,
-                           val market: String,
-                           val type: String? = null)
-
 data class ExchangeDto(val currency: Currency,
                        val market: String,
                        val stocks: List<StockDto>,
