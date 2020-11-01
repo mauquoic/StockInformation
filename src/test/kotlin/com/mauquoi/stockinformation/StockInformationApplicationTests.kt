71c1c79edac020279d0a612b6dd41f20abf1a900
mauquoic <mauquoic@gmail.com>
2020-11-01 15:44:41 +0100

    Embed kafka for tests
    Publish to kafka on server

--- src/test/kotlin/com/mauquoi/stockinformation/StockInformationApplicationTests.kt
+++ src/test/kotlin/com/mauquoi/stockinformation/StockInformationApplicationTests.kt
@@ -1,17 +1,20 @@
 package com.mauquoi.stockinformation
 
 import org.junit.jupiter.api.Test
+import org.springframework.boot.autoconfigure.kafka.KafkaAutoConfiguration
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.kafka.test.context.EmbeddedKafka
 import org.springframework.test.context.ActiveProfiles
 
+
 @EmbeddedKafka
 @ActiveProfiles("test")
-@SpringBootTest
+@SpringBootTest(properties = ["spring.kafka.bootstrap-servers=\${spring.embedded.kafka.brokers}"],
+        classes = [KafkaAutoConfiguration::class])
 class StockInformationApplicationTests {
 
-	@Test
-	fun contextLoads() {
-	}
+    @Test
+    fun contextLoads() {
+    }
 
 }
